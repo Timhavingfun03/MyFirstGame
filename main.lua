3cbea999a411154134d83e6ebc32e2ca7ddd65b6
Tim Torndal <tim.torndal@skola.botkyrka.se>
2021-02-22 16:57:14 +0100

    2

--- main.lua
+++ main.lua
@@ -10,47 +10,59 @@
 --     love.graphics.print(number)
 -- end
 
+
+
+
 function love.load()
     Px = 400
     Py = 390
+
+--borders
+B1 = 50
+B2 = 50
+B3 = 700
+B4 = 500
 
 end
 
 function love.update()
    
-    if (Py > 390)
-        then Py = 390 % love.graphics.getWidth()
+    if (Py > 540)
+        then Py = 540 % love.graphics.getWidth()
             end
     
-    if (Py < 110)
-        then Py = 110 % love.graphics.getWidth()
+    if (Py < 60)
+        then Py = 60 % love.graphics.getWidth()
          end   
 
-    if (Px < 210)
-        then Px = 210 % love.graphics.getWidth()
+    if (Px < 60)
+        then Px = 60 % love.graphics.getWidth()
          end
 
-     if (Px > 590)
-        then Px = 590 % love.graphics.getWidth()
+     if (Px > 740)
+        then Px = 740 % love.graphics.getWidth()
             end
-function love.keypressed(key)
-    if key == "down" then
-        Py = (Py + 20) % love.graphics.getWidth()
-        elseif key == "up" then Py = (Py - 20) % love.graphics.getWidth()
+
+    if love.keyboard.isDown("down") then
+        Py = (Py + 10) % love.graphics.getWidth()
+        elseif love.keyboard.isDown("up") then Py = (Py - 10) % love.graphics.getWidth()
     end
-    if key == "right" then
-        Px = (Px + 20) % love.graphics.getWidth()
-        elseif key == "left" then Px = (Px - 20) % love.graphics.getWidth()
+
+    if love.keyboard.isDown("right") then
+        Px = (Px + 10) % love.graphics.getWidth()
+        elseif love.keyboard.isDown("left") then Px = (Px - 10) % love.graphics.getWidth()
     end
-end
-    
-end
+end 
+
+
 
 
 function love.draw()
+
     love.graphics.setColor(255,255,255)
-    love.graphics.rectangle("line", 200, 100, 400, 300)
+    love.graphics.rectangle("line", B1, B2, B3, B4)
 
     love.graphics.setColor(255,0,0)
    player_1 = love.graphics.circle("fill", Px, Py, 10)
+
 end
